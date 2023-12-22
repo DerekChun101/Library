2f1b5e48fe6ab7ef28c052cffdc761d9793a4753
Derek Chun <derekchun2013@gmail.com>
2023-12-21 22:20:23 -0500

    Added some comments, change class names

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
 const myLibrary = [];
 const display = document.querySelector('.library');
 const bookform = document.querySelector('.book-form');
-function Book(name, author, pages, isReaded) {
+function Book(name, author, pages, isReaded) { // book constructor
     this.name = name;
     this.author = author;
     this.pages = pages;
     this.isReaded = isReaded;
 }
 
-function addBookToLibrary(e) {
-    e.preventDefault();
-    
-    const title = document.getElementById('title').value;
+function addBookToLibrary(e) { 
+    e.preventDefault();//prevents the page from reseting after submiting form
+    const title = document.getElementById('title').value; //gets all values from form
     const author = document.getElementById('Author').value;
     const pages = document.getElementById('pages').value;
     const read = document.getElementById('isReaded').checked;
-    const book = new Book(title, author, pages, read);
-    myLibrary.push(book);
-    displayLibrary(myLibrary);
-    bookform.reset();
+    const book = new Book(title, author, pages, read); //uses values to created new book
+    myLibrary.push(book);//push book into array
+    displayLibrary(myLibrary);// call display function to display array of books
+    bookform.reset();//clears form after submit
 }
 function displayLibrary(library) {
-    display.innerHTML = '';
-    library.forEach(book => {
+    display.innerHTML = '';//resets the display grid
+    library.forEach(book => { //creates a new book display for each book
         createBook(book);
     });
 }
 
 function createBook(book) {
-    let item = document.createElement('div'); 
-    item.setAttribute('class', 'book');
-    item.innerHTML = `<p>Title:${book.name}</p>
+    let bookCard = document.createElement('div'); 
+    bookCard.setAttribute('class', 'bookCard');
+    bookCard.innerHTML = `<p>Title:${book.name}</p>
                           <p>Author:${book.author}</p>
                           <p>Pages:${book.pages}</p>`;
     display.appendChild(item);
