ed6b9e4b3699a7991adb3c99af6e76089a81d0e9
Derek Chun <derekchun2013@gmail.com>
2023-12-22 12:57:18 -0500

    Added read status to book card

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 const myLibrary = [];
 const display = document.querySelector('.library');
 const bookform = document.querySelector('.book-form');
-function Book(name, author, pages, isReaded) { // book constructor
+function Book(name, author, pages, isRead) { // book constructor
     this.name = name;
     this.author = author;
     this.pages = pages;
-    this.isReaded = isReaded;
+    this.isRead = isRead;
 }
+
 
 function addBookToLibrary(e) { 
     e.preventDefault();//prevents the page from reseting after submiting form
     const title = document.getElementById('title').value; //gets all values from form
     const author = document.getElementById('Author').value;
     const pages = document.getElementById('pages').value;
-    const read = document.getElementById('isReaded').checked;
+    const read = document.getElementById('isRead').checked;
     const book = new Book(title, author, pages, read); //uses values to created new book
     myLibrary.push(book);//push book into array
     displayLibrary(myLibrary);// call display function to display array of books
+    console.log(myLibrary);
     bookform.reset();//clears form after submit
 }
 function displayLibrary(library) {
@@ -25,15 +27,35 @@ function displayLibrary(library) {
         createBook(book);
     });
 }
+function removeBook() {
 
+}
+function switchRead () {
+    
+}
 function createBook(book) {
     let bookCard = document.createElement('div'); 
+    let btnContainer = document.createElement('div');
     bookCard.setAttribute('class', 'bookCard');
-    bookCard.innerHTML = `<p>Title:${book.name}</p>
+    btnContainer.setAttribute('class', 'btnContainer');
+    let read = document.createElement('button');
+    read.setAttribute('class', 'readBtn');
+    if(book.isRead === true) {
+        read.classList.add('read');
+        read.textContent = 'Read'
+    } else {
+        read.classList.add('notRead')
+        read.textContent = 'Not Read'
+    }
+    bookCard.innerHTML = `<div class = 'book-info'>
+                         <p>Title:${book.name}</p>
                           <p>Author:${book.author}</p>
-                          <p>Pages:${book.pages}</p>`;
-    display.appendChild(item);
+                          <p>Pages:${book.pages}</p></div>`;
+    bookCard.appendChild(btnContainer);
+    btnContainer.appendChild(read);
+    display.appendChild(bookCard);
 }
+
 addEventListener('submit', addBookToLibrary);
 
 
